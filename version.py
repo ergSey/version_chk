12b55a1ca4e3d3b991e9d646470311d646687f92
ergSey <75573061+ergSey@users.noreply.github.com>
2021-03-26 12:47:51 +0300

    Update version.py

--- version.py
+++ version.py
@@ -6,119 +6,86 @@ HOST = "Zabbix_hostname"
 ZabS = "Zabix_server_ip"
 
 def zabbix_sender(item, value):
-        if os.path.isfile('/usr/bin/zabbix_sender') == True:
-                path = os.path.join(r'/usr/bin/', 'zabbix_sender')
-                command = '{0} -z {1} -s {2} -k {3} -o "{4}"'.format(path,ZabS,host,item,value)
-                pipe = os.popen(command)
-        elif os.path.isfile('C:\\zabbix\\bin\\win32\\zabbix_sender.exe') == True:
-                path = os.path.join(r'C:\zabbix\bin\win32', 'zabbix_sender.exe')
-                command = '{0} -z {1} -s {2} -k {3} -o "{4}"'.format(path,ZabS,HOST,item,value)
-                pipe = os.popen(command)
-        elif os.path.isfile('C:\\zabbix\\zabbix_sender.exe') == True:
-                path = os.path.join(r'C:\zabbix', 'zabbix_sender.exe')
-                command = '{0} -z {1} -s {2} -k {3} -o "{4}"'.format(path,ZabS,HOST,item,value)
-                pipe = os.popen(command)
+        for path in ['C:\\zabbix\\bin\\win32\\zabbix_sender.exe', 'C:\\zabbix\\zabbix_sender.exe', '/usr/bin/zabbix_sender']:
+                if os.path.isfile(path) == True: pipe = os.popen('{0} -z {1} -s {2} -k {3} -o "{4}"'.format(path,ZabS,host,item,value))
 
 #System 1
 if os.path.isfile('/etc/netact-release') == True:
-        command = 'cat /etc/netact-release | grep -v "CDA" | grep "SP" | tail -1 | awk -F\'-\' \'{print$1,"-",$3}\''
-        pipe = os.popen(command)
-        output = pipe.read()
+        output = os.popen('cat /etc/netact-release | grep -v "CDA" | grep "SP" | tail -1 | awk -F\'-\' \'{print$1,"-",$3}\'').read()
         item = "NMS.version"
         zabbix_sender(item, output)
 
-        command = 'hostnamectl | grep "Operating System" | awk -F\':\' \'{print$2}\''
-        pipe = os.popen(command)
-        output = pipe.read()
+        output = os.popen('hostnamectl | grep "Operating System" | awk -F\':\' \'{print$2}\'').read()
         item = "OS.version"
         zabbix_sender(item, output)
 
 #System 2
 elif os.path.isfile('/opt/oracle/ocsm/etc/iptego/display_version') == True:
-        command = 'cat /opt/oracle/ocsm/etc/iptego/display_version'
-        pipe = os.popen(command)
-        output = pipe.read()
+        output = os.popen('cat /opt/oracle/ocsm/etc/iptego/display_version').read()
         item = "NMS.version"
         output = re.sub("^\s+|\n|\r|\s+$", '', output)
         zabbix_sender(item, output)
 
-        command = 'hostnamectl | grep "Operating System" | awk -F\':\' \'{print$2}\''
-        pipe = os.popen(command)
-        output = pipe.read()
+        output = os.popen('hostnamectl | grep "Operating System" | awk -F\':\' \'{print$2}\'').read()
         item = "OS.version"
         output = re.sub("^\s+|\n|\r|\s+$", '', output)
         zabbix_sender(item, output)
 
 #System 3
 elif os.path.isfile('/etc/iskratel_installed_products.properties') == True:
-        command = 'cat /etc/iskratel_installed_products.properties | sed -n 1p'
-        pipe = os.popen(command)
-        output = pipe.read()
+        output = os.popen('cat /etc/iskratel_installed_products.properties | sed -n 1p').read()
         item = "NMS.version"
-        datastring = output.split('=')
-        data = datastring[1]
-        datastring = data.split(';')
-        output = datastring[len(datastring)-1]
-        output = re.sub("^\s+|\n|\r|\s+$", '', output)
+        datastring = output.split('=')[1].split(';')
+        output = re.sub("^\s+|\n|\r|\s+$", '', datastring[len(datastring)-1])
         output = output.split(',')
         output = output[1] + "," + output[2]
-        zabbix_sender(item, output)
+        zabbix_sender(item, output, HOST)
 
-        command = 'cat /etc/centos-release'
-        pipe = os.popen(command)
-        output = pipe.read()
+        output = os.popen('cat /etc/centos-release').read()
         item = "OS.version"
         output = re.sub("^\s+|\n|\r|\s+$", '', output)
-        zabbix_sender(item, output)
+        zabbix_sender(item, output, HOST)
 
 #System 4
 elif os.path.isfile('C:\\Program Files\\Nokia Solutions and Networks\\Traffica\\TrafRTTServer\\traffica.exe') == True:
         path = os.path.join(r'C:\Program Files\Nokia Solutions and Networks\Traffica\TrafRTTServer', 'traffica.exe')
         command = 'powershell.exe Get-ChildItem "{0}"'.format(path)
 
-        process = subprocess.Popen(["powershell","Get-Childitem \"C:\\Program Files\\Nokia Solutions and Networks\\Traffica\\TrafRTTServer\\traffica.exe\" | Get-ItemProperty | Select VersionInfo | Format-List *"],stdout=subprocess.PIPE);
-        result = process.communicate()[0]
-        result = str(result, 'utf-8')
-        result = list(result.split('\n'))
-        datastring = result[9]
-        datastring = datastring.split(':')
-        data = datastring[1]
-        data = re.sub("^\s+|\n|\r|\s+$", '', data)
+        process=subprocess.Popen(["powershell","Get-Childitem \"C:\\Program Files\\Nokia Solutions and Networks\\Traffica\\TrafRTTServer\\traffica.exe\" | Get-ItemProperty | Select VersionInfo | Format-List *"],stdout=subprocess.PIPE);
+        result=process.communicate()[0]
+        result = list(str(result, 'utf-8').split('\n'))
+        datastring = result[9].split(':')
+        data = re.sub("^\s+|\n|\r|\s+$", '', datastring[1])
         item = "NMS.version"
-        zabbix_sender(item, data)
-
-        process = subprocess.Popen(["powershell","Get-WmiObject -Class Win32_OperatingSystem | ForEach-Object -MemberName Caption"],stdout=subprocess.PIPE);
-        result = process.communicate()[0]
-        result = str(result, 'utf-8')
-        result = re.sub("^\s+|\n|\r|\s+$", '', result)
+        zabbix_sender(item, data, HOST)
+        
+        process=subprocess.Popen(["powershell","Get-WmiObject -Class Win32_OperatingSystem | ForEach-Object -MemberName Caption"],stdout=subprocess.PIPE);
+        result=process.communicate()[0]
+        result = re.sub("^\s+|\n|\r|\s+$", '', str(result, 'utf-8'))
         item = "OS.version"
-        zabbix_sender(item, result)
+        zabbix_sender(item, result, HOST)
 
 #System 5
 elif os.path.isfile('/etc/SuSE-release') == True:
         command = 'cat /etc/SuSE-release | sed -n 1p'
-        pipe = os.popen(command)
-        output = pipe.read()
+        output = os.popen('cat /etc/SuSE-release | sed -n 1p').read()
         item = "OS.version"
         output = re.sub("^\s+|\n|\r|\s+$", '', output)
-        zabbix_sender(item, output)
+        zabbix_sender(item, output, HOST)
 
         command = 'export PATH=$PATH:/opt/webserver/programs/proton/db/bin; source /home/webserverpt/.bashrc; su - webserverpt -c "psql -d pt -U webserver -c \'select * from portal.tbl_productinfo;\' | sed -n 3p"'
         pipe = os.popen(command)
         output = pipe.read()
         item = "NMS.version"
         output = re.sub("^\s+|\n|\r|\s+$", '', output)
-        output = output.split('|')
-        output = output[3]
-        zabbix_sender(item, output)
+        output = output.split('|')[3]
+        for host in [HOST_CSProbe, HOST]:
+                zabbix_sender(item, output, host)
 
 #System 6
 else:
-        command = 'wmic os get Caption, CSDVersion'
-        pipe = os.popen(command)
-        output = pipe.read()
+        output = os.popen('wmic os get Caption, CSDVersion').read()
         result = list(output.split('\n'))
         item = "OS.version"
-        output = result[1]
-        output = re.sub("^\s+|\n|\r|\s+$", '', output)
+        output = re.sub("^\s+|\n|\r|\s+$", '', result[1])
         zabbix_sender(item, output)
